70a60577b4a056b1fee2ccb8550958df5719fc3d
DELL <DELL@10.0.0.179>
2023-04-07 10:33:43 -0400

    initial commit

---
+++ src/demo/Test.java
@@ -0,0 +1,16 @@
+package demo;
+
+public class Test {
+
+	public static void main (String args[]) {
+		
+		
+		System.out.println("test..........");
+		
+		System.out.println("github lecture...");
+		
+	}
+	
+	
+	
+}
